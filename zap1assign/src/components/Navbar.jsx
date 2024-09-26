56dfbca2be91e57ca0246aab5108fa0e4bcdcc2e
mcken-freelancer <mcken-freelancer@users.noreply.github.com>
2024-09-26 12:16:27 +0000

    hmmm

--- zap1assign/src/components/Navbar.jsx
+++ zap1assign/src/components/Navbar.jsx
@@ -2,28 +2,34 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <>
-      <div className="flex bg-green-600 justify-between text-white">
+      <div className="flex bg-green-600 justify-between text-white border-solid border-black">
         <h3>555-0198 Maple Street, Spriengfield, IL 62701</h3>
         <h3>Monday-Saturday:9am-11.30pm</h3>
       </div>
+
       <div>
-        <ul>
-          <li>
-            <Link to="/"> Home</Link>
-          </li>
-          <li>
-            <Link to="/about"> About</Link>
-          </li>
-          <li>
-            <Link to="/services"> Services</Link>
-          </li>
-          <li>
-            <Link to="/blog"> Blog</Link>
-          </li>
-          <li>
-            <Link to="/contact"> Contact</Link>
-          </li>
-        </ul>
+        <div>
+          <image src="./assets/Main Logo.svg" />
+        </div>
+        <div>
+          <ul className="flex">
+            <li>
+              <Link to="/"> Home</Link>
+            </li>
+            <li>
+              <Link to="/about"> About</Link>
+            </li>
+            <li>
+              <Link to="/services"> Services</Link>
+            </li>
+            <li>
+              <Link to="/blog"> Blog</Link>
+            </li>
+            <li>
+              <Link to="/contact"> Contact</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
