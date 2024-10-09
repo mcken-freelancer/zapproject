e70170b8422c6625bb395d35c2356d6369e0e6c8
mcken-freelancer <mcken-freelancer@users.noreply.github.com>
2024-10-09 14:44:19 +0000

    navdone

--- zap2assign/src/App.jsx
+++ zap2assign/src/App.jsx
@@ -3,7 +3,52 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>Hello you Motherf*ckers!!</div>
+      <div className="entirepage">
+        <div className="navbarwrapper">
+          <div className="navbarnav">
+            <div className="navbarrow">
+              <div className="navcontainer">
+                <p className="logotype">
+                  <img src="/freshfieldlogo.png" alt="freshfield" />
+                </p>
+                <nav className="navmenu">
+                  <div className="navcenter">
+                    <div className="alignhorizontalcenter">
+                      <a href="#about" className="navlink">
+                        About Us
+                      </a>
+                      <a href="#ourstory" className="navlink">
+                        Our Story
+                      </a>
+                      <a href="#ourharvest" className="navlink">
+                        Our Harvest
+                      </a>
+                      <a href="#benefits" className="navlink">
+                        Benefits
+                      </a>
+                      <a href="#reviews" className="navlink">
+                        Reviews
+                      </a>
+                      <a href="#contactus" className="navlink">
+                        Contact Us
+                      </a>
+                    </div>
+                  </div>
+                  <div>
+                    <a
+                      href="https://webflow.com/templates"
+                      target="_blank"
+                      className="navbutton"
+                    >
+                      Buy Template
+                    </a>
+                  </div>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
